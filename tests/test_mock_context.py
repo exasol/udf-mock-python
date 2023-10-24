81abae735e2d44f8a3ae5e7559eeb942b327d76a
Mikhail Beck <mikhail.beck@exasol.com>
2023-10-24 14:07:12 +0100

    Split MockContext into Standalone and multi-group (#42)
    * Split MockContext into Standalone and multi-group
    
    * Fixed compatibility issue with the scalar parameters being wrapped in a container.
    
    * Update exasol_udf_mock_python/mock_context.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update exasol_udf_mock_python/mock_context.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update exasol_udf_mock_python/mock_context.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update exasol_udf_mock_python/mock_context.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Refactoring following a review
    
    * Making the MockContext tests running under Python 3.8
    
    * Updated MockContext test_scroll test
    
    * validate_emit is taken out of the StandaloneMockContext class
    
    ---------
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>

---
+++ tests/test_mock_context.py
@@ -0,0 +1,69 @@
+import pytest
+import pandas as pd
+
+from exasol_udf_mock_python.group import Group
+from exasol_udf_mock_python.mock_context import MockContext
+from tests.test_mock_context_standalone import meta_set_emits
+
+
+@pytest.fixture
+def context_set_emits(meta_set_emits):
+    pets = Group([(1, 'cat'), (2, 'dog')])
+    bugs = Group([(3, 'ant'), (4, 'bee'), (5, 'beetle')])
+    groups = [pets, bugs]
+    return MockContext(iter(groups), meta_set_emits)
+
+
+def test_scroll(context_set_emits):
+    groups = []
+    while context_set_emits.next_group():
+        group = [context_set_emits.t2]
+        while context_set_emits.next():
+            group.append(context_set_emits.t2)
+        groups.append(group)
+    assert groups == [['cat', 'dog'], ['ant', 'bee', 'beetle']]
+
+
+def test_output_groups(context_set_emits):
+    context_set_emits.next_group()
+    context_set_emits.emit(1, 'cat')
+    context_set_emits.emit(2, 'dog')
+    context_set_emits.next_group()
+    context_set_emits.emit(3, 'ant')
+    context_set_emits.emit(4, 'bee')
+    context_set_emits.emit(5, 'beetle')
+    context_set_emits.next_group()
+    assert len(context_set_emits.output_groups) == 2
+    assert context_set_emits.output_groups[0] == Group([(1, 'cat'), (2, 'dog')])
+    assert context_set_emits.output_groups[1] == Group([(3, 'ant'), (4, 'bee'), (5, 'beetle')])
+
+
+def test_output_groups_partial(context_set_emits):
+    context_set_emits.next_group()
+    context_set_emits.emit(1, 'cat')
+    context_set_emits.emit(2, 'dog')
+    context_set_emits.next_group()
+    context_set_emits.emit(3, 'ant')
+    context_set_emits.emit(4, 'bee')
+    assert len(context_set_emits.output_groups) == 2
+    assert context_set_emits.output_groups[0] == Group([(1, 'cat'), (2, 'dog')])
+    assert context_set_emits.output_groups[1] == Group([(3, 'ant'), (4, 'bee')])
+
+
+def test_no_context_exception(context_set_emits):
+
+    for _ in range(3):
+        context_set_emits.next_group()
+
+    with pytest.raises(RuntimeError):
+        _ = context_set_emits.t2
+    with pytest.raises(RuntimeError):
+        _ = context_set_emits.get_dataframe()
+    with pytest.raises(RuntimeError):
+        context_set_emits.next()
+    with pytest.raises(RuntimeError):
+        _ = context_set_emits.size()
+    with pytest.raises(RuntimeError):
+        context_set_emits.reset()
+    with pytest.raises(RuntimeError):
+        context_set_emits.emit(1, 'cat')
---
+++ tests/test_mock_context_standalone.py
@@ -0,0 +1,104 @@
+import pytest
+import pandas as pd
+
+from exasol_udf_mock_python.column import Column
+from exasol_udf_mock_python.mock_meta_data import MockMetaData
+from exasol_udf_mock_python.mock_context import StandaloneMockContext, validate_emit
+
+
+def udf_wrapper():
+    pass
+
+
+@pytest.fixture
+def meta_scalar_return():
+    return MockMetaData(
+        script_code_wrapper_function=udf_wrapper,
+        input_type='SCALAR',
+        input_columns=[Column('t', int, 'INTEGER')],
+        output_type='RETURNS',
+        output_columns=[Column('t', int, 'INTEGER')]
+    )
+
+
+@pytest.fixture
+def meta_set_emits():
+    return MockMetaData(
+        script_code_wrapper_function=udf_wrapper,
+        input_type='SET',
+        input_columns=[Column('t1', int, 'INTEGER'), Column('t2', str, 'VARCHAR(100)')],
+        output_type='EMITS',
+        output_columns=[Column('t1', int, 'INTEGER'), Column('t2', str, 'VARCHAR(100)')]
+    )
+
+
+@pytest.fixture
+def context_scalar_return(meta_scalar_return):
+    return StandaloneMockContext((5,), meta_scalar_return)
+
+
+@pytest.fixture
+def context_set_emits(meta_set_emits):
+    return StandaloneMockContext([(5, 'abc'), (6, 'efgh')], meta_set_emits)
+
+
+def test_get_dataframe(context_set_emits):
+    df = context_set_emits.get_dataframe()
+    expected_df = pd.DataFrame({'t1': [5, 6], 't2': ['abc', 'efgh']})
+    pd.testing.assert_frame_equal(df, expected_df)
+
+
+def test_get_dataframe_limited(context_set_emits):
+    df = context_set_emits.get_dataframe(1, 1)
+    expected_df = pd.DataFrame({'t2': ['abc']})
+    pd.testing.assert_frame_equal(df, expected_df)
+
+
+def test_attr_set(context_set_emits):
+    assert context_set_emits.t1 == 5
+    assert context_set_emits.t2 == 'abc'
+
+
+def test_attr_scalar(context_scalar_return):
+    assert context_scalar_return.t == 5
+
+
+def test_next(context_set_emits):
+    assert context_set_emits.next()
+    assert context_set_emits.t1 == 6
+    assert context_set_emits.t2 == 'efgh'
+
+
+def test_next_end(context_set_emits):
+    context_set_emits.next()
+    assert not context_set_emits.next()
+
+
+def test_reset(context_set_emits):
+    context_set_emits.next()
+    context_set_emits.reset()
+    assert context_set_emits.t1 == 5
+    assert context_set_emits.t2 == 'abc'
+
+
+def test_size(context_set_emits):
+    assert context_set_emits.size() == 2
+
+
+def test_validate_emit_good(meta_set_emits):
+    validate_emit((10, 'fish'), meta_set_emits.output_columns)
+
+
+def test_validate_emit_bad(meta_set_emits):
+    with pytest.raises(Exception):
+        validate_emit((10,), meta_set_emits.output_columns)
+    with pytest.raises(Exception):
+        validate_emit((10, 'fish', 4.5), meta_set_emits.output_columns)
+    with pytest.raises(Exception):
+        validate_emit((10., 'fish'), meta_set_emits.output_columns)
+
+
+def test_emit_df(context_set_emits):
+    df = pd.DataFrame({'t1': [1, 2], 't2': ['cat', 'dog']})
+    context_set_emits.emit(df)
+    assert context_set_emits.output == [(1, 'cat'), (2, 'dog')]
